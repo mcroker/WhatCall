3368bfee0bdc21f36ab7a8b3c579cba88f394d12
James Croker <JamesCroker01@gmail.com>
2025-12-23 20:22:50 +0000

    initial db query

---
+++ src/app/videos.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+// Import the functions you need from the SDKs you need
+import { FirebaseApp, initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD4-v5htNmy6ocVhw7d46uzcTpFYOVqzMs",
+  authDomain: "whatcall-52d6a.firebaseapp.com",
+  projectId: "whatcall-52d6a",
+  storageBucket: "whatcall-52d6a.firebasestorage.app",
+  messagingSenderId: "139166244778",
+  appId: "1:139166244778:web:4f0914dc3a5eca506955d3",
+  measurementId: "G-TDXW9BDJ3G"
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Videos {
+
+  private firebaseApp: FirebaseApp;
+
+  constructor() {
+    // Initialize Firebase
+    this.firebaseApp = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(this.firebaseApp);
+  }
+
+  async getVideos(): Promise<any[]> {
+    const db = getFirestore(this.firebaseApp);
+    const querySnapshot = await getDocs(collection(db, "videos"));
+    return querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data()}));
+  }
+
+}
