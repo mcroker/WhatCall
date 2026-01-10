8d27d970404022ee71e3f699140893b5ea6d4565
James Croker <JamesCroker01@gmail.com>
2026-01-10 21:01:41 +0000

    config firebase

--- src/services/profileService.spec.ts
+++ src/services/profileService.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ProfileService } from './profile-service';
+import { ProfileService } from './profileService';
 
 describe('ProfileService', () => {
   let service: ProfileService;
